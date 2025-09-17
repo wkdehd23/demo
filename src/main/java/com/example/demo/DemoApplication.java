8f65d959e2fcdd7c0f32691630cdcef60bfb439a
wkdehd <jack9350@sungkyul.ac.kr>
2025-09-17 09:59:02 +0900

    DAY2_UPDATE

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.example.demo; // 현재폴더위치
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication; // 스프링핵심클래스
+import org.springframework.boot.autoconfigure.SpringBootApplication; // 자동설정기능활성화
 
-@SpringBootApplication
-public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+@SpringBootApplication // 애노테이션(스프링부트APP 명시, 하위다양한설정을자동등록)
+ public class DemoApplication { // 클래스이름
+	public static void main(String[] args) { // 메인메서드(프로그램시작점)
+		SpringApplication.run(DemoApplication.class, args); // run 메서드로실행
 	}
-
 }
--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,13 +1,13 @@
 package com.example.demo;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller // 컨트롤러 어노테이션 명시
 public class DemoController {
-    @GetMapping(" /hello")
-    public String hello (Model model){
-        model.addAttribute("data", "방갑습니다.");
-        return "hello";
+    @GetMapping("/hello") // 전송 방식 GET
+    public String hello(Model model) {
+        model.addAttribute("data", " 방갑습니다."); // model 설정
+        return "hello"; // hello.html 연결
     }
 }
